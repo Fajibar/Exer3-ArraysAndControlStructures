d6e01b37a0bf48a835c003f3b67857300c335699
Fajibar <60416236+Fajibar@users.noreply.github.com>
2020-02-28 06:01:02 +0000

    Update Palindrome.java

--- src/com/lotus/jbc/basicjava/Palindrome.java
+++ src/com/lotus/jbc/basicjava/Palindrome.java
@@ -13,7 +13,15 @@ public class Palindrome {
 	 */
 	public boolean determinePalindrome(char[] chars) {
 		
-		// Modify content of this method
-		return false;
+		int i = 0, j = str.length() - 1;
+                while (i < j) { 
+                if (str.charAt(i) != str.charAt(j))
+                     return false; 
+                     i++; 
+                     j--; 
+                } 
+                return true; 
+    
+		
 	}
 }
